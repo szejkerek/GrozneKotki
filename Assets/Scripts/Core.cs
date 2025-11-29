a6b29ccb0905eb707d18b3f1cb242e8f4ee2edb7
Bartek <bartekk.gordon@gmail.com>
2025-11-30 00:06:24 +0100

    Assembly def

--- Assets/Scripts/Core.cs
+++ Assets/Scripts/Core.cs
@@ -1,6 +1,18 @@
+using System;
 using UnityEngine;
 
-public class Core : MonoBehaviour
+public class Core : MonoBehaviour, IDamagable
 {
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            TakeDamage(1);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        Debug.Log("Taking damage");
+    }
 }
