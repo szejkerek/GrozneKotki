09a0f284b04dd9d9c3ff2387d6338cdd474505a6
juliwro851 <72439273+juliwro851@users.noreply.github.com>
2025-11-30 06:18:19 +0100

    ui

--- Assets/Scripts/healthUI.cs
+++ Assets/Scripts/healthUI.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.InputSystem;
 
 public class healthUI : MonoBehaviour
 {
@@ -10,6 +12,27 @@ public class healthUI : MonoBehaviour
     public Color inactive;
     public int activeCount;
     public int maxHealth;
+    public Vector3 initialTransform;
+
+    public void Start()
+    {
+        initialTransform = this.transform.position;
+        Debug.Log(initialTransform);
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y-500, this.transform.position.z);
+    }
+
+    private void Update()
+    {
+        var keyboard = Keyboard.current;
+
+        if (keyboard.oKey.wasPressedThisFrame)
+            LoadHealthDisplay();
+    }
+
+    public void LoadHealthDisplay()
+    {
+        this.transform.DOMove(initialTransform, 0.3f);
+    }
 
     public void FullHealthUI()
     {
