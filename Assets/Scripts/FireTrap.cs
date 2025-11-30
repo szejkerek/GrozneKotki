82709a829ec3c7cbd9a0eafbe223f9b0fffc713e
Bartek <bartekk.gordon@gmail.com>
2025-11-30 09:50:28 +0100

    Update

--- Assets/Scripts/FireTrap.cs
+++ Assets/Scripts/FireTrap.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,7 @@ public class FireTrap : MonoBehaviour
     public float lifeTime = 6f;
     public float damage = 80f;
     public float blastRadius = 4f;
-    public GameObject hitEffect;
+    public bool debugDamage = true;
 
     bool activated;
 
@@ -19,14 +20,6 @@ public class FireTrap : MonoBehaviour
 
     private void Update()
     {
-        if (activated) return;
-        activated = true;
-
-        if (hitEffect != null)
-        {
-            Instantiate(hitEffect, transform.position, Quaternion.identity);
-        }
-
         Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
 
         foreach (Collider col in colliders)
@@ -34,9 +27,30 @@ public class FireTrap : MonoBehaviour
             IDamagable damagable = col.GetComponent<IDamagable>();
             if (damagable != null)
             {
-                damagable.TakeDamage(damage*Time.deltaTime);
+                float frameDamage = damage * Time.deltaTime;
+                damagable.TakeDamage(frameDamage);
+
+#if UNITY_EDITOR
+                if (debugDamage)
+                {
+                    Debug.Log(
+                        $"[FireTrap] {name} dealt {frameDamage:F2} damage to {col.name}",
+                        this
+                    );
+                }
+#endif
             }
         }
     }
 
+    private void OnDrawGizmos()
+    {
+        // Filled sphere for visual area
+        Gizmos.color = new Color(1f, 0.5f, 0f, 0.15f);
+        Gizmos.DrawSphere(transform.position, blastRadius);
+
+        // Wireframe outline
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, blastRadius);
+    }
 }
